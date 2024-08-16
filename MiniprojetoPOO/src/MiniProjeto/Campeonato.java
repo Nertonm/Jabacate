0c9d545a39155fc331292763e5f6643069542581
Nerton <thiago2nerton@gmail.com>
2024-08-16 18:49:02 -0300

    Changes in jogarPartida

--- MiniprojetoPOO/src/MiniProjeto/Campeonato.java
+++ MiniprojetoPOO/src/MiniProjeto/Campeonato.java
@@ -11,12 +11,20 @@ public class Campeonato {
 	private void jogarPartida(Clube m, Clube v) {
 		/* irá sortear um placar como sendo dois inteiros entre 0 e 5 representando a quantidade de gols que cada time marcou. 
 		Após esse sorteio, o método verifica o resultado, chamando os respectivos métodos para ganhar, empatar e perder dos dois clubes */
+		
         Random random = new Random();
 		int golsM = random.nextInt(6);
 		int golsV = random.nextInt(6);
 		if (golsM == golsV) {
+			m.empatar();
+			v.empatar();
 		}
 		if (golsM >= golsV) {
+			m.ganhar(golsM - golsV);
+			v.perder(golsV - golsM);
+		} else {
+			m.perder(golsM - golsV);
+			v.ganhar(golsV - golsM);
 		}
 
 	}
