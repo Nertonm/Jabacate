f92acf0c0f17ea0a9bc2b4f8cfce698500c52636
Nerton <thiago2nerton@gmail.com>
2024-08-16 16:13:54 -0300

    Starting mini project

---
+++ MiniprojetoPOO/src/MiniProjeto/Clube.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package MiniProjeto;
+
+/*
+  A classe Clube tem como atributos seu nome, seu número de pontos e seu saldo de gols. Os métodos ganhar, 
+  empatar e perder devem incrementar os pontos e o saldo de gols de acordo com as regras do campeonato.
+
+ */
+public class Clube {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
